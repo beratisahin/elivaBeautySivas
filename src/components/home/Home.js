ce407e61157612f44c5eee2ba5c7eb466bc5ed40
beratisahin <beratisahin94@gmail.com>
2023-05-06 21:02:31 +0300

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,93 @@
+import React from "react";
+import "./Home.css";
+import NedenBercislina from "../home/neden/Neden.js";
+import Owl from "../slider/owlcarousel/Owl";
+import Klinik from "../klinik/Klinik.js";
+import Social from "../iletisim/Social";
+
+import TopluHizmet from "../topluhizmet/TopluHizmet.js";
+
+function Home() {
+  return (
+    <div>
+      <div className="row">
+        <h2
+          className="doktorlarTitle"
+          style={{
+            textAlign: "center",
+            fontSize: "x-large",
+            marginTop: "1rem",
+            color: "#b22e86",
+            fontFamily: "sans-serif",
+          }}
+        >
+          Kampanyalarımız Devam Etmektedir
+        </h2>
+        <Klinik />
+      </div>
+
+      <hr />
+      <div className="row">
+        <TopluHizmet />
+      </div>
+
+      <hr />
+      <div
+        style={{ margin: "auto", display: "block", justifyContent: "center" }}
+      >
+        <Social />
+      </div>
+      <hr />
+      <div className="row">
+        <h2
+          className="doktorlarTitle"
+          style={{
+            textAlign: "center",
+            fontSize: "x-large",
+            color: "#b22e86",
+            fontFamily: "sans-serif",
+          }}
+        >
+          Hizmetlerimiz
+        </h2>
+        <Owl />
+      </div>
+      <hr />
+
+      <div className="row m-2">
+        <div
+          className="col-lg-6 col-md-6 col-sm-6 col-12"
+          style={{ marginTop: "2.5rem" }}
+        >
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d421.17744069932326!2d37.01621061423902!3d39.749928352986196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x407eab26c1c35085%3A0x41c0276a8592a8cc!2zS2F2dWtjdSDEsMWfIEhhbsSx!5e0!3m2!1str!2str!4v1683396027593!5m2!1str!2str"
+            className="googleharita"
+            style={{
+              width: "100%",
+              height: "45rem",
+              frameBorder: "0",
+              allowFullScreen: "",
+              referrerpolicy: "no-referrer-when-downgrade",
+              ariaHidden: "false",
+              tabIndex: "0",
+              border: "2px #b22e86 solid",
+              paddingBottom: "0.5rem",
+              paddingTop: "0.5rem",
+            }}
+          >
+            {" "}
+          </iframe>
+        </div>
+
+        <div
+          className="col-lg-6 col-md-6 col-sm-6 col-12"
+          style={{ marginTop: "2.5rem" }}
+        >
+          <NedenBercislina />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/iletisim/Social.js
@@ -0,0 +1,88 @@
+import "./Iletisim.css";
+import React from "react";
+import useDocumentTitle from "../../useDocumentTitle.js";
+import Title from "../title/Title";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import ContactMailIcon from "@material-ui/icons/ContactMail";
+import HomeIcon from "@material-ui/icons/Home";
+import i1 from "./1.webp";
+import i2 from "./2.webp";
+import i3 from "./3.webp";
+import i4 from "./4.webp";
+import i5 from "./5.webp";
+import i6 from "./6.webp";
+
+function Iletisim() {
+  useDocumentTitle("Sivas Lazer Epilasyon - Sivas Eliva Beauty Salonu ");
+  return (
+    <div className="row">
+      <div class="col-lg-2s col-md-2 col-sm-12 col-12 p-5">
+        <a
+          href="https://goo.gl/maps/4bkTx9yDzQrPCMX37?coh=178571&entry=tt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={i1} className="contactIcons" title="Adres için İkona Tıklayınız"></img>
+        </a>
+        <p className="contactText">
+          <span style={{color:"#E4439E"}}><b>Adres:</b></span><br/>
+          Eskikale Mah. Osmanpaşa Cad. 13/5 Sok. Kavukçu İşhanı No: 2/102 Kat: 1  <br/> <b>Merkez / Sivas</b>
+        </p>
+      </div>
+      <div class="col-lg-2s col-md-2 col-sm-12 col-12 p-5">
+        <a
+          href="tel: +905073660558"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={i2} className="contactIcons" title="Rezervasyon için İkona Tıklayınız"></img>
+        </a>
+        <p className="contactText">
+        <span style={{color:"#E4439E"}}><b>Rezervasyon:</b></span><br/>
+          <b>0507 366 05 58</b>
+        </p>
+      </div>
+      <div class="col-lg-2s col-md-2 col-sm-12 col-12 p-5">
+        <img src={i3} className="contactIcons" title="Çalışma Saatleri"></img>
+        <p className="contactText">
+         <span style={{color:"#E4439E"}}><b>Çalışma Saatleri:</b></span><br/>
+          Her Gün: 09:00 – 19:00
+        </p>
+      </div>
+      <div class="col-lg-2s col-md-2 col-sm-12 col-12 p-5">
+      <a href="https://wa.me/+905073660558" target="_blank">
+          <img src={i4} className="contactIcons" title="WhatsApp için İkona Tıklayınız"></img>
+        </a>
+        <p className="contactText">
+        <span style={{color:"#E4439E"}}><b>WhatsApp:</b></span><br/>
+          <b>0507 366 05 58</b>
+        </p>
+      </div>
+      <div class="col-lg-2s col-md-2 col-sm-12 col-12 p-5">
+        <a href="https://www.instagram.com/elivabeauty.sivas/" target="_blank">
+          <img src={i5} className="contactIcons" title="Instagram için İkona Tıklayınız"></img>
+        </a>
+        <p className="contactText">
+        <span style={{color:"#E4439E"}}><b>Instagram:</b></span><br/>
+          @elivabeauty.sivas
+        </p>
+      </div>
+      <div class="col-lg-2s col-md-2 col-sm-12 col-12 p-5">
+        <a
+          href="https://www.facebook.com/elivabeautysivas"
+          target="_blank"
+        >
+          <img src={i6} className="contactIcons" title="Facebook için İkona Tıklayınız"></img>
+        </a>
+        <p className="contactText">
+        <span style={{color:"#E4439E"}}><b>Facebook:</b></span><br/>
+          @elivabeautysivas
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Iletisim;
